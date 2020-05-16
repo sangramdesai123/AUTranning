90cd60628c66a5071abb95098d5fbcc71ebf4541
sangramdesai123 <sangram.desai@somaiya.edu>
2020-05-16 13:04:13 +0530

    day3 collections

---
+++ src/day3/BracketCount.java
@@ -0,0 +1,35 @@
+package day3;
+
+import java.util.Stack;
+
+public class BracketCount {
+	static boolean balancedParenthensies(String s) {
+		Stack<Character> stack  = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '[' || c == '(' || c == '{' ) {     
+                stack.push(c);
+            } else if(c == ']') {
+                if(stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if(c == ')') {
+                if(stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }           
+            } else if(c == '}') {
+                if(stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+		
+	}
+	public static void main(String args[]) {
+
+        System.out.println(balancedParenthensies("{()}{"));
+        System.out.println(balancedParenthensies("{([])}"));
+    }
+}
